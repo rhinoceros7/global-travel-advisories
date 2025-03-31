192bdca084d1aa8b8bacf2224cddad028bdde5e6
Ryan King <ryan.r.king7@gmail.com>
2025-03-31 14:30:57 -0400

    Update API link

--- src/api/advisoryService.js
+++ src/api/advisoryService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "https://travel-advisory-map-api.onrender.com/";
+const API_BASE = "https://travel-advisory-map-api.onrender.com";
 
 export async function getAllSummaries()
 {
